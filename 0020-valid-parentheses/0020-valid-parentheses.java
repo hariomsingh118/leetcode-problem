09465b96596c856578e8d4a05749f1352fbb15a9
Hariom Singh <hariomsinghrajput236@gmail.com>
2026-06-27 22:02:17 +0530

    Time: 3 ms (87.27%), Space: 43.3 MB (56.97%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            // Push opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            // Check closing brackets
+            else {
+
+                // No opening bracket available
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
