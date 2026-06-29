f02d99de490b5f27b2ded95420af8f8d06e9176d
Hariom Singh <hariomsinghrajput236@gmail.com>
2026-06-29 14:55:10 +0530

    Time: 1 ms (99.83%), Space: 46.7 MB (99.86%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int [] arr = new int[nums.length] ;
+        int count = 0 ;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i]!=0){
+               arr[count] = nums[i];
+                count++;
+            }
+        }
+        for(int i = 0 ; i < nums.length ; i ++){
+           nums[i]=arr[i];
+        }
+    }
+}
